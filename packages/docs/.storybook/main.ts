764af4c7446600b6f80c503aa3e059e50a04f01f
Alvaro Neto <alvaroneto.dev@gmail.com>
2023-12-22 22:06:51 -0300

    feature: added a11y for check accessibility

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -12,13 +12,14 @@ function getAbsolutePath(value: string): any {
 const config: StorybookConfig = {
   stories: [
     "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx"
+    "../src/stories/**/*.stories.tsx",
   ],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/nextinhuh.png',
     alt: 'Nextinhuh',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
